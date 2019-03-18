9969c391f199a372a3c9c6e87d955c110012b034
PsyNut <48391846+psynut@users.noreply.github.com>
2019-03-18 00:12:54 -0700

    GUI Stays Visible

--- Assets/Scripts/UpdateInputField.cs
+++ Assets/Scripts/UpdateInputField.cs
@@ -8,25 +8,11 @@ public class UpdateInputField : MonoBehaviour {
 
     public InputField mainInputField;
     private Calendar calendar;
-    private Canvas dateCanvas;
-
-    void Start(){
-		  dateCanvas = GameObject.Find("Date Canvas").GetComponent<Canvas>();
-    	  if(!dateCanvas){
-    	    Debug.LogWarning("Did not find 'Date Canvas' canvas component!");
-      	}
-      	dateCanvasEnabled(false);
-    }
 
     public void UpdateField()
     {
 		int dayNumb = EventSystem.current.currentSelectedGameObject.GetComponent<DayButton>().CurrentNumber();
 		calendar = GameObject.FindObjectOfType<Calendar>();
        	mainInputField.text = calendar.ReturnDate(dayNumb);
-		dateCanvasEnabled(false);
-    }
-
-    public void dateCanvasEnabled(bool canvasbool){
-    	dateCanvas.enabled = canvasbool;
     }
 }
